d92c7848a91b0cd2351ad8b93ccb5b552252b4e3
Chris Douglas <cdouglas@apache.org>
2022-09-14 00:52:08 -0700

    Ugh. what.

--- hydroflow/tests/surface_echo.rs
+++ hydroflow/tests/surface_echo.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
 use std::time::Duration;
 
 use hydroflow::hydroflow_syntax;
 use hydroflow::scheduled::graph::Hydroflow;
-use tokio::sync::mpsc::UnboundedSender;
+// completely broken... hope not in a way that matters
+//use tokio::sync::mpsc::UnboundedSender;
+//use tracing_subscriber::prelude::*;
 
 #[test]
 pub fn test_echo() {
@@ -38,162 +41,162 @@ pub fn test_echo() {
     std::thread::sleep(Duration::from_secs(1));
 }
 
-#[derive(Clone)]
-pub struct UnboundedSenderEq<T>(pub UnboundedSender<T>);
-impl<T> PartialEq for UnboundedSenderEq<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.same_channel(&other.0)
-    }
-}
-impl<T> Eq for UnboundedSenderEq<T> {}
-
-// pub struct UnboundedReceiverEq<T>(UnboundedReceiver<T>);
-// impl<T> PartialEq for UnboundedReceiverEq<T> {
-//     fn eq(&self, other: &Self) -> bool {
-//         std::ptr::eq(self, other)
-//     }
-// }
-// impl<T> Eq for UnboundedReceiverEq<T> {}
-
-#[test]
-pub fn test_shuffle_client_server() {
-    let server = tokio::sync::mpsc::unbounded_channel::<String>();
-    let mut c_sendv = Vec::new();
-    let mut c_recvv = Vec::new();
-    for _ in 0..10 {
-        let (send, recv) = tokio::sync::mpsc::unbounded_channel::<String>();
-        c_sendv.push(UnboundedSenderEq(send));
-        c_recvv.push(recv);
-    }
-    let s_recv = server.1;
-    let mut s_df = hydroflow_syntax! {
-        // TODO: how to find the client that sent on this channel? Needs to be in the data?
-        members = recv_iter(c_sendv);
-        data = recv_stream(s_recv) -> map(|line| { println!("Broadcast {}", line); line });
-        my_join = join();
-        data -> map(|d| ((), d)) -> [0]my_join;
-        members -> map(|m| ((), m)) -> [1]my_join;
-        my_join -> for_each(|((), (d, UnboundedSenderEq(m)))| { m.send(d).unwrap(); });
-    };
-
-    // collection of Hydroflow clients; each sends a message to the server (need Rc handle)
-    let mut c_dfs = c_recvv
-    .into_iter()
-    .enumerate()
-    .map(|(i, recv)| {
-        let server_h = UnboundedSenderEq(server.0.clone());
-        server_h.0.send(i.to_string().to_owned()).unwrap();
-        let df = hydroflow_syntax! {
-            // TODO: how to get the server handle w/in here, send?
-            recv_stream(recv) -> for_each(|line| { println!("{} recv {}", i, line); });
-        };
-        println!(
-            "{}",
-            df.serde_graph()
-                .expect("No graph found, maybe failed to parse.")
-                .to_mermaid()
-        );
-        df
-    })
-    .collect::<Vec<_>>();
-    s_df.run_available();
-    for df in c_dfs.iter_mut() {
-        df.run_available();
-    }
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct NodeId {
+    name: String,
 }
 
-#[test]
-#[ignore] // doesn't terminte
-pub fn test_shuffle_all_to_all() {
-    // // initialize 10 nodes
-    // let mut channel_vec: Vec<(UnboundedSenderEq<String>, UnboundedReceiverEq<String>)> = (1..10)
-    //     .map(|_| tokio::sync::mpsc::unbounded_channel::<String>())
-    //     .map(|(send, recv)| (UnboundedSenderEq(send), UnboundedReceiverEq(recv)))
-    //     .collect();
+#[derive(Clone, Debug)]
+struct NodeStart {
+    id: NodeId,
+    clock: VC,
+    peers: Vec<NodeId>,
+}
 
-    let mut send_vec = Vec::new();
-    let mut recv_vec = Vec::new();
-    for _ in 0..10 {
-        let (send, recv) = tokio::sync::mpsc::unbounded_channel::<String>();
-        send_vec.push(UnboundedSenderEq(send));
-        recv_vec.push(recv);
+impl NodeStart {
+    fn empty(id: &'static str, peer_names: Vec<&'static str>) -> Self {
+        Self::new(id, VC::new(), peer_names)
     }
-
-    let mut dfs = recv_vec
-        .into_iter()
-        .enumerate()
-        .map(|(i, recv)| {
-            let sends = send_vec
-                .iter()
-                .enumerate()              // includes the position in the iterator as a tuple
-                .filter(|&(j, _)| i != j) // so it can filter itself out
-                .map(|(_, s)| s)
-                .cloned()                 // apparently this is OK, avoids Rc?
-                .collect::<Vec<_>>();     // all the members this will send to (i.e., excluding itself)
-            let df = hydroflow_syntax! {
-                members = recv_iter(sends);
-                data = recv_stream(recv) -> map(|line| format!("Recv at {}: {}", i, line)) -> map(|line| { println!("{}", line); line });
-                my_join = join();
-                data -> map(|d| ((), d)) -> [0]my_join;
-                members -> map(|m| ((), m)) -> [1]my_join;
-                my_join -> for_each(|((), (d, UnboundedSenderEq(m)))| { m.send(d).unwrap(); });
-            };
-            println!(
-                "{}",
-                df.serde_graph()
-                    .expect("No graph found, maybe failed to parse.")
-                    .to_mermaid()
-            );
-            df
-        })
-        .collect::<Vec<_>>();
-
-    send_vec[5].0.send("my message".to_owned()).unwrap();
-
-    loop {
-        for df in dfs.iter_mut() {
-            df.run_available();
+    fn new(id: &'static str, clock: VC, peer_names: Vec<&'static str>) -> Self {
+        NodeStart {
+            id: NodeId { name: id.to_string()},
+            clock,
+            peers: peer_names.into_iter().map(|p| NodeId { name: p.to_string() }).collect(),
         }
     }
-
-    // df.run_available();
-
-    // lines_send.send("Hello".to_owned()).unwrap();
-    // lines_send.send("World".to_owned()).unwrap();
-    // df.run_available();
-
-    // lines_send.send("Hello".to_owned()).unwrap();
-    // lines_send.send("World".to_owned()).unwrap();
-    // df.run_available();
-
-    // // Allow background thread to catch up.
-    // std::thread::sleep(Duration::from_secs(1));
 }
 
-// async fn async_test() {
-//     LocalSet::new()
-//         .run_until(async {
-//             let (a_send, a_recv) = tokio::sync::mpsc::unbounded_channel::<usize>();
-//             let (b_send, b_recv) = tokio::sync::mpsc::unbounded_channel::<usize>();
+#[derive(Clone, Debug)]
+struct VC {
+    clock: HashMap<String, u64>,
+}
 
-//             tokio::task::spawn_local(async move {
-//                 let mut flow = hydroflow_syntax! {
-//                     recv_stream(a_recv) -> for_each(|x| { b_send.send(x).unwrap(); });
-//                 };
-//                 flow.run_async().await.unwrap();
-//             });
-//             tokio::task::spawn_local(async move {
-//                 let mut flow = hydroflow_syntax! {
-//                     recv_stream(b_recv) -> for_each(|x| println!("{}", x));
-//                 };
-//                 flow.run_async().await.unwrap();
-//             });
+impl VC {
+    pub fn new() -> Self {
+        VC {
+            clock: HashMap::new()
+        }
+    }
+}
 
-//             a_send.send(1).unwrap();
-//             a_send.send(2).unwrap();
-//             a_send.send(3).unwrap();
+#[derive(Clone, Debug)]
+enum Pkt<T> {
+    // request catchup beyond <known>
+    HELLO {
+        src: NodeId,
+        dst: NodeId,
+        known: VC,
+    },
+    // 
+    BROADCAST {
+        src: NodeId,
+        seq: VC,
+        data: T,
+    },
+}
 
-//             tokio::task::yield_now().await;
-//         })
-//         .await;
-// }
+#[test]
+pub fn book_example() {
+    let (pairs_send, pairs_recv) = hydroflow::util::unbounded_channel::<(usize, usize)>();
+
+    let mut flow = hydroflow_syntax! {
+        // inputs: the origin vertex (node 0) and stream of input edges
+        origin = recv_iter(vec![0]);
+        stream_of_edges = recv_stream(pairs_recv);
+        reached_vertices = merge();
+        origin -> [0]reached_vertices;
+
+        // the join
+        my_join_tee = join() -> map(|(_src, ((), dst))| dst) -> tee();
+        reached_vertices -> map(|v| (v, ())) -> [0]my_join_tee;
+        stream_of_edges -> [1]my_join_tee;
+
+        // the loop and the output
+        my_join_tee[0] -> [1]reached_vertices;
+        my_join_tee[1] -> for_each(|x| println!("Reached: {}", x));
+    };
+
+    println!(
+        "{}",
+        flow.serde_graph()
+            .expect("No graph found, maybe failed to parse.")
+            .to_mermaid()
+    );
+    pairs_send.send((0, 1)).unwrap();
+    pairs_send.send((2, 4)).unwrap();
+    pairs_send.send((3, 4)).unwrap();
+    pairs_send.send((1, 2)).unwrap();
+    pairs_send.send((0, 3)).unwrap();
+    pairs_send.send((0, 3)).unwrap();
+    flow.run_available();
+}
+
+#[test]
+pub fn test_all_to_all_unif() {
+    // channel for adding nodes to the flow
+    let (node_send, node_recv) = hydroflow::util::unbounded_channel::<NodeStart>();
+    // channel for messages on the network, entering the flow
+    let (df_send, df_recv) = hydroflow::util::unbounded_channel::<Pkt<String>>();
+    let mut df = hydroflow_syntax! {
+        // active nodes
+        nodes = merge();
+
+        // stream of nodes into the system
+        init = join() -> for_each(|(dstId, (clock, peer))| {
+            println!("DEBUG ({:?}, ({:?}, {:?}))", dstId, clock, peer);
+        });
+        new_node = recv_stream(node_recv) -> tee();
+        // add self
+        new_node[0] -> map(|n| { println!("{:?} joined", n); (n.id, n.clock) }) -> [0]nodes;
+        // join peers against active nodes
+        nodes -> [0]init;
+        new_node[1] -> flat_map(move |hello| hello.peers.into_iter().map(move |dst| (dst, hello.id.clone())))
+                    -> [1]init;
+        //init -> for_each(|(dstId, (clock, peer))| {
+        //    println!("DEBUG {:?}", x);
+        //});
+        
+        // messages passed between nodes (can also introduce traffic from handle outside)
+        //ntwk = recv_stream(df_recv) -> tee();
+
+        //// messages in the system
+        //messages = merge();
+
+        //// HELLO
+        //hello_msg = join();
+        //messages -> [0]hello_msg;
+        //ntwk[0] -> filter_map(|h: Pkt<String>| {
+        //    match h {
+        //        Pkt::HELLO { src, dst, known } => Some(((), (src, known))),
+        //        _ => None
+        //    }
+        //}) -> [1]hello_msg;
+
+        //hello_msg -> for_each(|h| {
+        //    println!("HELLO to {:?}", h);
+        //});
+
+        // BROADCAST
+        //bc_msg = join();
+        //ntwk[1] -> filter_map(|h: Pkt<String>| {
+        //    match h {
+        //        Pkt::BROADCAST { src, seq, data } => Some(((), (src, seq))),
+        //        _ => None
+        //    }
+        //}) -> for_each(|h: ((), (NodeId, VC))| {
+        //    println!("HELLO to {:?}", h.1.0);
+        //});
+
+    };
+    node_send.send(NodeStart::empty("server", vec!["a", "b", "c"])).unwrap();
+    node_send.send(NodeStart::empty("a", vec!["server"])).unwrap();
+    node_send.send(NodeStart::empty("b", vec!["server"])).unwrap();
+    node_send.send(NodeStart::empty("c", vec!["server"])).unwrap();
+    df_send.send(Pkt::HELLO {
+        src: NodeId { name: "a".to_string(), },
+        dst: NodeId { name: "b".to_string(), },
+        known: VC { clock: HashMap::new(), }
+    }).unwrap();
+
+    df.run_available();
+}
