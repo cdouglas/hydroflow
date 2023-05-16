86f3fc01319976e5bcd487c918211bfef02af32b
Chris Douglas <cdouglas@apache.org>
2023-05-16 15:22:30 -0700

    echoserver changes

--- hydroflow/examples/echoserver/protocol.rs
+++ hydroflow/examples/echoserver/protocol.rs
@@ -1,7 +1,7 @@
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(PartialEq, Clone, Serialize, Deserialize, Debug)]
+#[derive(PartialEq, Eq, Hash, Clone, Serialize, Deserialize, Debug)]
 pub struct EchoMsg {
     pub payload: String,
     pub ts: DateTime<Utc>,
